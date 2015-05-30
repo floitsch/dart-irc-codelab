fcff94dd2e020eb240d2d714f23f5ef4e74045db
Florian Loitsch <floitsch@google.com>
2015-05-30 03:03:05 +0200

    First socket connection.

--- main.dart
+++ main.dart
@@ -2,6 +2,15 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'dart:io';
+
 void main() {
-  print("Hello world");
+  var future = Socket.connect("localhost", 6667);
+  // Now register a callback:
+  future.then((socket) {
+    // The socket is now available.
+    print("Connected");
+    socket.destroy();
+  });
+  print("Callback has been registered, but we are not connected yet");
 }
